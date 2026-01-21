a04d07fbee2d4a3f829b59bd65fb7bce28735207
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-21 16:03:44 +0530

    feat: Add Primitives.java program

---
+++ src/com/stschool/java/fundamentals/Primitives.java
@@ -0,0 +1,14 @@
+package com.stschool.java.fundamentals;
+
+public class Primitives {
+    public static void main(String[] args0) {
+        byte age = 21;
+        short year = 2026;
+        int salary = 500000;
+        long netWorth = 2300404000000L;
+        System.out.println("Age : "+ age);
+        System.out.println("Year : "+ year);
+        System.out.println("Salary : "+ salary);
+        System.out.println("Networth : "+ netWorth);
+    }
+}
