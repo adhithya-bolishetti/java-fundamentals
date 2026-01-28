85619051ef92113c3c41274a16fb9b70722df1a3
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-28 09:20:36 +0530

    feat: Add program for printing sum of first N natural numbers

---
+++ src/com/stschool/java/fundamentals/SumNaturalNumbers.java
@@ -0,0 +1,18 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class SumNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int sum = 0;
+        for(int i = 1; i <= num; i++) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
+}
