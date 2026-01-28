2b5a0d9210814dad231a93b977785b60528874ff
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-28 09:18:39 +0530

    feat: Add program for printing 1 to N numbers

---
+++ src/com/stschool/java/fundamentals/Print1ToN.java
@@ -0,0 +1,14 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class Print1ToN {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= num; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
