d8599c7b66f058bb235bfbe395549535fdf3989f
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-28 09:23:44 +0530

    feat: Add program for printing digits in a number

---
+++ src/com/stschool/java/fundamentals/PrintDigits.java
@@ -0,0 +1,19 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class PrintDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        while(num != 0) {
+            int digit = num % 10;
+            System.out.println(digit);
+            num = num / 10;
+        }
+
+        sc.close();
+    }
+}
