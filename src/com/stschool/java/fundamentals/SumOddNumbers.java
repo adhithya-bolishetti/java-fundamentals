b7aead35d7d33e4f40749b13ae1fb67f9c1a3cbc
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-28 09:21:48 +0530

    feat: Add program for printing sum of first N odd numbers

---
+++ src/com/stschool/java/fundamentals/SumOddNumbers.java
@@ -0,0 +1,20 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class SumOddNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int sum = 0;
+        for(int i = 1; i <= num; i++) {
+            if(i % 2 != 0) {
+                sum = sum + i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
