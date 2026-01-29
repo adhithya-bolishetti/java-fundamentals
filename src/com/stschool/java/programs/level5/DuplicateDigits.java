2c92de6348568910340c3d9d2e97558c9395f5dd
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-29 17:19:15 +0530

    feat: Add program to find duplicate digits in a number

---
+++ src/com/stschool/java/programs/level5/DuplicateDigits.java
@@ -0,0 +1,27 @@
+package com.stschool.java.programs.level5;
+
+import java.util.Scanner;
+
+public class DuplicateDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        int[] freq = new int[10];
+
+        while(n != 0) {
+            int rem = n % 10;
+            freq[rem]++;
+            n /= 10;
+        }
+
+        System.out.print("Duplicate Numbers : ");
+        for(int i=0;i<10;i++) {
+            if(freq[i] > 1) System.out.print(i + " ");
+        }
+
+        sc.close();
+    }
+}
