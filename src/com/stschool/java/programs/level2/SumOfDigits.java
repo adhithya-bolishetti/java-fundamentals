1a47aac770e79921ac70323b1e9f6c1d825d14d7
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-01-29 14:45:08 +0530

    feat: Add program to print sum of digits

---
+++ src/com/stschool/java/programs/level2/SumOfDigits.java
@@ -0,0 +1,22 @@
+package com.stschool.java.programs.level2;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        int sum = 0;
+
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+
+        sc.close();
+    }
+}
