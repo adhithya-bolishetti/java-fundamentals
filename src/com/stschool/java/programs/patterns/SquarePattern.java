7849daec86d83dd22090fcedb822c819fdec45f2
Adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-02-02 23:15:44 +0530

    feat: Add program to print Square pattern

---
+++ src/com/stschool/java/programs/patterns/SquarePattern.java
@@ -0,0 +1,25 @@
+package com.stschool.java.programs.patterns;
+
+import java.util.Scanner;
+
+public class SquarePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
