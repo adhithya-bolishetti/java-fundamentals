362af36a7fbcca4e0d796c441a9015b5fcfeca47
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-09 11:14:14 +0530

    feat: Add reverse of a string program

---
+++ src/com/stschool/java/ReverseString.java
@@ -0,0 +1,18 @@
+package com.stschool.java;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String s = scanner.next();
+        String result = "";
+
+        for(int i=s.length()-1;i>=0;i--) {
+            result += s.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + result);
+    }
+}
