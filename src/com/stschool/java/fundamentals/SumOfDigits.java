9a97bbe8e9059d740d83d4ed561e424de8a95385
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-09 20:08:58 +0530

    feat: Add program to find sum of digits of a number

---
+++ src/com/stschool/java/fundamentals/SumOfDigits.java
@@ -0,0 +1,20 @@
+package com.stschool.java.fundamentals;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+        int n = scanner.nextInt();
+
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
