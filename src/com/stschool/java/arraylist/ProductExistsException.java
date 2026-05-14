26607c478ada9f0a9fe210ed5aa757adaa40a060
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-14 21:58:52 +0530

    feat: Add ProductExistsException

---
+++ src/com/stschool/java/arraylist/ProductExistsException.java
@@ -0,0 +1,7 @@
+package com.stschool.java.arraylist;
+
+public class ProductExistsException extends Exception{
+    public ProductExistsException(String message) {
+        super(message);
+    }
+}
