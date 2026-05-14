fcd416cd9e5ad1eb390272dea0d8aa7f97a857bb
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-14 21:59:51 +0530

    feat: Add product repository interface

---
+++ src/com/stschool/java/arraylist/ProductRepositoryInterface.java
@@ -0,0 +1,12 @@
+package com.stschool.java.arraylist;
+
+import java.util.ArrayList;
+
+public interface ProductRepositoryInterface {
+    Product save(Product product) throws ProductExistsException;
+    Product findById(String id) throws ProductNotFoundException;
+    ArrayList<Product> findAll();
+    Product update(Product product) throws ProductNotFoundException;
+    boolean delete(Product product) throws ProductNotFoundException;
+    boolean deleteById(String id) throws ProductNotFoundException;
+}
