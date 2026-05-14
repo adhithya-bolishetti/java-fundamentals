215c8c5a01fab071c76efd485841f41a9c92d029
adhithya-bolishetti <adhithya.bolishetti@gmail.com>
2026-05-14 21:59:09 +0530

    feat: Add ProductNotFoundException

---
+++ src/com/stschool/java/arraylist/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.stschool.java.arraylist;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
